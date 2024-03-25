6aa22c75a88dc8bd3d294235c9f139fd86977de5
hossam <hossameddine.djennad@gmail.com>
2024-03-26 00:06:44 +0100

    add first componets

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <LearnigResources v-for="Resource in savedResources" :key="Resource.id"></LearnigResources>
+</template>
+
+<script>
+import LearnigResources from './componets/LearnigResources/LearnigResources.vue'
+export default {
+    components:{
+        LearnigResources
+    },
+data(){
+    return{
+        savedResources:[
+            {
+             id:'vuejs',
+             title:'documents',
+             description:'official documents',
+             linke:'vuejs.org'
+            },
+            {
+             id: 'google',
+             title: 'documents google',
+            description: 'google documents',
+            linke: 'google.com'
+            }
+        ]
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/componets/LearnigResources/LearnigResources.vue
@@ -0,0 +1,26 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3>{{ title }}</h3>
+                <button>Delete</button>
+            </header>
+        </div>
+        <p>{{ description }}</p>
+        <nav>
+            <a :href="linke">vue linek</a>
+        </nav>
+    </li>
+
+</template>
+
+<script>
+export default {
+    props: ['title','description','linke']
+
+}
+</script>
+
+<style>
+
+</style>
